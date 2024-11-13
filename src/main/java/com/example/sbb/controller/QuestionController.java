60625c2ed7fb5f64070867f4cb469519a670c816
BaekEuiHun <euihunbaek@gmail.com>
2024-11-13 17:54:17 +0900

    질문등록하기 controller 생성

--- src/main/java/com/example/sbb/controller/QuestionController.java
+++ src/main/java/com/example/sbb/controller/QuestionController.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -38,6 +41,19 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    //질문 등록하기 페이지 불러오기
+    @GetMapping("/question/create")
+    public String questionCreate() {
+        return "question_form";
+    }
+
+    //질문 등록하기
+    @PostMapping("/question/create")
+    public String questionCreate(@RequestParam(value = "subject") String subject, @RequestParam(value = "content") String content) {
+        this.questionService.create(subject, content);
+        return "redirect:/question/list";
     }
 
 }
