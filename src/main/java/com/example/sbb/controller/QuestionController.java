1faece97ee0491b5bb8dea48749fa2e8d80c8f41
BaekEuiHun <euihunbaek@gmail.com>
2024-11-19 17:59:22 +0900

    질문 수정 controller 추가

--- src/main/java/com/example/sbb/controller/QuestionController.java
+++ src/main/java/com/example/sbb/controller/QuestionController.java
@@ -10,6 +10,8 @@ import com.example.sbb.service.QuestionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,8 +19,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.security.Principal;
 import java.util.List;
+
+
 
 @RequiredArgsConstructor
 @Controller
@@ -61,6 +67,19 @@ public class QuestionController {
         }
         this.questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/question/modify/{id}")
+    public String questionModify(QuestionForm questionForm, @PathVariable Integer id, Principal principal) {
+        Question question = this.questionService.getQuestion(id);
+        if(!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"수정 권한이 없습니다.");
+
+        }
+        questionForm.setSubject(question.getSubject());
+        questionForm.setContent(question.getContent());
+        return "question_form";
     }
 
 }
